155a6c8884c1fba0ff79a19e0e6359eddf76d7bd
natan1503 <natan1503@hotmail.com>
2016-03-30 10:44:14 -0300

    Adicionando Entidades

--- src/br/edu/ifpb/entidade/Eleitor.java
+++ src/br/edu/ifpb/entidade/Eleitor.java
@@ -2,16 +2,24 @@ package br.edu.ifpb.entidade;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_eleitor")
 @NamedQuery(name = "Eleitor.getAll", query = "from Eleitor")
-public class Eleitor extends Pessoa{
-	
 
+public class Eleitor extends Pessoa {
+	
+	@Column(name = "sessao_eleitor")
+	private int sessao;
+
+	public int getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(int sessao) {
+		this.sessao = sessao;
+	}
+	
 }
