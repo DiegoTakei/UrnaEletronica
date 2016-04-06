16de15fe6a7e2ea6d9c907a8331dc2966509d16b
Natha-Gabriel <nathagabriel38@gmail.com>
2016-04-06 16:15:37 -0300

    Update Apuracao.java

--- src/br/edu/ifpb/entidade/Apuracao.java
+++ src/br/edu/ifpb/entidade/Apuracao.java
@@ -6,37 +6,35 @@ public class Apuracao {
 	
 	private int id;
 	
-	private int qnt_votos;
+	private int QuantidadeVotos;
 
-	private int qnt_brancos;
+	private int QuantidadeBrancos;
 	
-	private int qnt_nulos;
+	private int QuantidadeNulos;
 	
-	private int qnt_ausentes;
+	private int QuantidadeAusentes;
 	
-	private Candidato prefeito_mais_votado;
+	private Candidato PrefeitoMaisVotado;
 	
-	private Candidato governador_mais_votado;
+	private Candidato GovernadorMaisVotado;
 	
-	private Candidato presidente_mais_votado;
+	private Candidato PresidenteMaisVotado;
 	
-	private Candidato prefeito_menos_votado;
+	private Candidato PrefeitoMenosVotado;
 	
-	private Candidato governador_menos_votado;
+	private Candidato GovernadorMenosVotado;
 	
-	private Candidato presidente_menos_votado;
+	private Candidato PresidenteMenosVotado;
 	
-	private Candidato prefeito_eleito;
+	private Candidato prefeitoEleito;
 	
-	private Candidato governador_eleito;
+	private Candidato GovernadorEleito;
 	
-	private Candidato presidente_eleito;
+	private Candidato PresidenteEleito;	
 	
+	private Candidato CandidatosSegundoTurno;
 	
-	private Candidato candidatos_segundo_turno;
-	
-	
-	
+
 	public int getId() {
 		return id;
 	}
@@ -45,116 +43,117 @@ public class Apuracao {
 		this.id = id;
 	}
 
-	public int getQnt_votos() {
-		return qnt_votos;
+	public int getQuantidadeVotos() {
+		return QuantidadeVotos;
 	}
 
-	public void setQnt_votos(int qnt_votos) {
-		this.qnt_votos = qnt_votos;
+	public void setQuantidadeVotos(int quantidadeVotos) {
+		QuantidadeVotos = quantidadeVotos;
 	}
 
-	public int getQnt_brancos() {
-		return qnt_brancos;
+	public int getQuantidadeBrancos() {
+		return QuantidadeBrancos;
 	}
 
-	public void setQnt_brancos(int qnt_brancos) {
-		this.qnt_brancos = qnt_brancos;
+	public void setQuantidadeBrancos(int quantidadeBrancos) {
+		QuantidadeBrancos = quantidadeBrancos;
 	}
 
-	public int getQnt_nulos() {
-		return qnt_nulos;
+	public int getQuantidadeNulos() {
+		return QuantidadeNulos;
 	}
 
-	public void setQnt_nulos(int qnt_nulos) {
-		this.qnt_nulos = qnt_nulos;
+	public void setQuantidadeNulos(int quantidadeNulos) {
+		QuantidadeNulos = quantidadeNulos;
 	}
 
-	public int getQnt_ausentes() {
-		return qnt_ausentes;
+	public int getQuantidadeAusentes() {
+		return QuantidadeAusentes;
 	}
 
-	public void setQnt_ausentes(int qnt_ausentes) {
-		this.qnt_ausentes = qnt_ausentes;
+	public void setQuantidadeAusentes(int quantidadeAusentes) {
+		QuantidadeAusentes = quantidadeAusentes;
+	}
+
+	public Candidato getPrefeitoMaisVotado() {
+		return PrefeitoMaisVotado;
+	}
+
+	public void setPrefeitoMaisVotado(Candidato prefeitoMaisVotado) {
+		PrefeitoMaisVotado = prefeitoMaisVotado;
+	}
+
+	public Candidato getGovernadorMaisVotado() {
+		return GovernadorMaisVotado;
+	}
+
+	public void setGovernadorMaisVotado(Candidato governadorMaisVotado) {
+		GovernadorMaisVotado = governadorMaisVotado;
+	}
+
+	public Candidato getPresidenteMaisVotado() {
+		return PresidenteMaisVotado;
+	}
+
+	public void setPresidenteMaisVotado(Candidato presidenteMaisVotado) {
+		PresidenteMaisVotado = presidenteMaisVotado;
+	}
+
+	public Candidato getPrefeitoMenosVotado() {
+		return PrefeitoMenosVotado;
+	}
+
+	public void setPrefeitoMenosVotado(Candidato prefeitoMenosVotado) {
+		PrefeitoMenosVotado = prefeitoMenosVotado;
+	}
+
+	public Candidato getGovernadorMenosVotado() {
+		return GovernadorMenosVotado;
+	}
+
+	public void setGovernadorMenosVotado(Candidato governadorMenosVotado) {
+		GovernadorMenosVotado = governadorMenosVotado;
+	}
+
+	public Candidato getPresidenteMenosVotado() {
+		return PresidenteMenosVotado;
+	}
+
+	public void setPresidenteMenosVotado(Candidato presidenteMenosVotado) {
+		PresidenteMenosVotado = presidenteMenosVotado;
+	}
+
+	public Candidato getPrefeitoEleito() {
+		return prefeitoEleito;
+	}
+
+	public void setPrefeitoEleito(Candidato prefeitoEleito) {
+		this.prefeitoEleito = prefeitoEleito;
+	}
+
+	public Candidato getGovernadorEleito() {
+		return GovernadorEleito;
+	}
+
+	public void setGovernadorEleito(Candidato governadorEleito) {
+		GovernadorEleito = governadorEleito;
+	}
+
+	public Candidato getPresidenteEleito() {
+		return PresidenteEleito;
+	}
+
+	public void setPresidenteEleito(Candidato presidenteEleito) {
+		PresidenteEleito = presidenteEleito;
+	}
+
+	public Candidato getCandidatosSegundoTurno() {
+		return CandidatosSegundoTurno;
+	}
+
+	public void setCandidatosSegundoTurno(Candidato candidatosSegundoTurno) {
+		CandidatosSegundoTurno = candidatosSegundoTurno;
 	}
 	
-	public Candidato getPrefeito_mais_votado() {
-		return prefeito_mais_votado;
-	}
-
-	public void setPrefeito_mais_votado(Candidato prefeito_mais_votado) {
-		this.prefeito_mais_votado = prefeito_mais_votado;
-	}
-
-	public Candidato getGovernador_mais_votado() {
-		return governador_mais_votado;
-	}
-
-	public void setGovernador_mais_votado(Candidato governador_mais_votado) {
-		this.governador_mais_votado = governador_mais_votado;
-	}
-
-	public Candidato getPresidente_mais_votado() {
-		return presidente_mais_votado;
-	}
-
-	public void setPresidente_mais_votado(Candidato presidente_mais_votado) {
-		this.presidente_mais_votado = presidente_mais_votado;
-	}
-
-	public Candidato getPrefeito_menos_votado() {
-		return prefeito_menos_votado;
-	}
-
-	public void setPrefeito_menos_votado(Candidato prefeito_menos_votado) {
-		this.prefeito_menos_votado = prefeito_menos_votado;
-	}
-
-	public Candidato getGovernador_menos_votado() {
-		return governador_menos_votado;
-	}
-
-	public void setGovernador_menos_votado(Candidato governador_menos_votado) {
-		this.governador_menos_votado = governador_menos_votado;
-	}
-
-	public Candidato getPresidente_menos_votado() {
-		return presidente_menos_votado;
-	}
-
-	public void setPresidente_menos_votado(Candidato presidente_menos_votado) {
-		this.presidente_menos_votado = presidente_menos_votado;
-	}
-
-	public Candidato getPrefeito_eleito() {
-		return prefeito_eleito;
-	}
-
-	public void setPrefeito_eleito(Candidato prefeito_eleito) {
-		this.prefeito_eleito = prefeito_eleito;
-	}
-
-	public Candidato getGovernador_eleito() {
-		return governador_eleito;
-	}
-
-	public void setGovernador_eleito(Candidato governador_eleito) {
-		this.governador_eleito = governador_eleito;
-	}
-
-	public Candidato getPresidente_eleito() {
-		return presidente_eleito;
-	}
-
-	public void setPresidente_eleito(Candidato presidente_eleito) {
-		this.presidente_eleito = presidente_eleito;
-	}
-
-	public Candidato getCandidatos_segundo_turno() {
-		return candidatos_segundo_turno;
-	}
-
-	public void setCandidatos_segundo_turno(Candidato candidatos_segundo_turno) {
-		this.candidatos_segundo_turno = candidatos_segundo_turno;
-	}
 
 }
